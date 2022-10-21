857434a3c2020bbd2ff90ccb3360079e8177fa90
mgvictoriano <mgvictoriano@live.com>
2022-10-21 20:41:09 -0300

    Add Pokemon Data

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.css']
 })
-export class PokemonCardComponent implements OnInit {
 
-  constructor() { }
+export class PokemonCardComponent {
 
-  ngOnInit(): void {
+  @Input()
+  public pokemon: Pokemon;
+
+  public leadingZero(str: String | number, size: number): string {
+    let s = String(str);
+
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+
+    return s;
   }
 
 }
